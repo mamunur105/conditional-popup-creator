a8b297885664d43f7b6076c2488e10f2ad9718e7
mamun <rmamunur105@gmail.com>
2019-09-03 02:21:36 +0600

    popups creator last update 03-sept-2019

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,19 +14,10 @@
             var delay =  modalelement[index].getAttribute('data-delay');
             var datatitle =  modalelement[index].getAttribute('data-title');
             var delaymillisecond = delay * 1000;
-            
-            // var getstorage = localStorage.getItem("close_modal");
-            // localStorage.setItem("close_modal", '');
-
-            console.log(getstorage);
-            for (i in getstorage) {
-                // return ;
-            }
-
             if(dataexit == 'exit'){
                 delaymillisecond = 0;
             }
-            // console.log(delayinmilisec);
+
             modals[index] = new jBox('Modal', {
                 closeButton: false, // Remove close button
                 closeOnClick: false, // Remove close on click events
@@ -35,51 +26,21 @@
                 title: '<div class="data-title">'+datatitle+'</div>',
                 minWidth: 600,
                 maxWidth:800,
-                // maxHeight:700,
                 content: datacontant,
                 onCreated: function () {
                     this.content.find('.close-modal').on('click', function() {
                     this.close();
-                    // localStorage.setItem("close_modal", modals[index]);
                     }.bind(this));
                 }
-                // open:funjction
 
             });
             if(dataexit == 'pageload'){
-
                 modals[index].open();
-                
             }else{
                 exitmodal.push(modals[index]);
-                // console.log(exitmodal.length);
-              
+ 
             }
         } 
-        if (exitmodal.length > 0 && !popupsdisplayed) {
-            window.onbeforeunload = function() {
-                if(!popupsdisplayed){
-                    for (i in exitmodal) {
-                        exitmodal[i].open();//
-                    }
-                    popupsdisplayed = true;
-                    return true;
-                } 
-                return false;   
-            }
-            
-            
-        }
-
-        
-        // onload
-    
-
-
-
-
-
-
     });
     
 
